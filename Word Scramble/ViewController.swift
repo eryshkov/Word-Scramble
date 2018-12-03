ceda10db7ca2c66243fe526d2d21a43e00edd6b2
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-03 21:36:43 +0300

    error messages implemented

--- Word Scramble/ViewController.swift
+++ Word Scramble/ViewController.swift
@@ -51,6 +51,9 @@ class ViewController: UITableViewController {
     func submit(answer: String) {
         let lowerAnswer = answer.lowercased()
         
+        let errorTitle: String
+        let errorMessage: String
+        
         if isPossible(word: lowerAnswer) {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
@@ -58,9 +61,26 @@ class ViewController: UITableViewController {
                     
                     let indexPath = IndexPath(row: 0, section: 0)
                     tableView.insertRows(at: [indexPath], with: .automatic)
+                    return
+                }else{
+                    //Not real
+                    errorTitle = "Word not recognised"
+                    errorMessage = "You can't just make them up, you know!"
                 }
+            }else{
+                //Not original
+                errorTitle = "Word used already"
+                errorMessage = "Be more original!"
             }
+        }else{
+            //Not possible
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell word from '\(title!.lowercased())'"
         }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac,animated: true)
     }
     
     func isPossible(word: String) -> Bool {
