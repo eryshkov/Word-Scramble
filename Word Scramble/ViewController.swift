7900499a9c38f0426d58b70b43b99d56ddc8483c
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-12-03 17:47:03 +0300

    Initial Commit

---
+++ Word Scramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Scramble
+//
+//  Created by Evgeniy Ryshkov on 03/12/2018.
+//  Copyright © 2018 Evgeniy Ryshkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
